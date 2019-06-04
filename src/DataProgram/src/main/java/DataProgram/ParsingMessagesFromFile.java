c0f057cf17f5cf2799b7c1eb43d6ee555c57602c
Vianney Doleans <vianney.doleans@epitech.eu>
2019-06-04 15:27:28 +0900

    Fix parsing : add banList + optimization parsing

--- src/DataProgram/src/main/java/DataProgram/ParsingMessagesFromFile.java
+++ src/DataProgram/src/main/java/DataProgram/ParsingMessagesFromFile.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,31 +16,49 @@ class ParsingMessagesFromFile {
     private List<String> messages;
     private long nb = 0;
     private Pattern hourFound;
+    private List<String> banList;
+
+    private boolean checkBanList(String line)
+    {
+        for (String ban : banList)
+        {
+            if (line.contains(ban))
+                return false;
+        }
+        return true;
+    }
 
     private void parser(Path path)
     {
-        hourFound = Pattern.compile(".*([01]?[0-9]|2[0-3]):[0-5][0-9].*");
+        hourFound = Pattern.compile("\\d+ \\w+ \\d{4}, \\d{1,2}:\\d{2} [AP]M");
 
         try {
-
             Files.lines(path, StandardCharsets.UTF_8)
-                    .map(line -> line.split("(AM,)|'(PM,)"))
+                    .map(line -> line.split(": ", 2))
                     .forEach(container ->
                     {
                         this.nb += 1;
-                        for (String message : container) {
-                            Matcher hour = hourFound.matcher(message);
-                           if (!(message.equals("") ||
-                                   hour.matches() ||
-                                   message.contains("AM") ||
-                                   message.contains("PM") ||
-                                   nb < 5)) {
-                               if (message.charAt(0) == ',')
-                                   message = message.substring(1);
-                               messages.add(message);
-                           }
+                        String message;
+                        if (container.length > 1) {
+                            message = container[1];
+                            if (!(nb < 3) && checkBanList(message)) {
+                                messages.add(message);
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("enter");
+                            if (container.length > 0) {
+                                System.out.println(container[0]);
+                                Matcher hour = hourFound.matcher(container[0]);
+                                if (messages.size() > 0 && !hour.matches() && checkBanList(container[0])) {
+                                    System.out.println("done");
+                                    messages.set(messages.size() - 1, messages.get(messages.size() - 1) + "\n" + container[0]);
+                                }
+                            }
                         }
                     });
+
         }
         catch (Exception ex)
         {
@@ -54,9 +73,15 @@ class ParsingMessagesFromFile {
         return this.messages;
     }
 
+    private void initBanList()
+    {
+        this.banList = Arrays.asList("Emoticons", "Photo", "Video");
+    }
+
     ParsingMessagesFromFile(String path)
     {
         this.messages = new ArrayList<>();
+        initBanList();
         parser(Paths.get(path));
     }
 }
