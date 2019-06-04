9a8fd3fbca575f28326f68d55c4ded7e60405fdd
Sugi <sugiuro.dev@gmail.com>
2019-06-04 15:45:16 +0900

    quick preprocessing for spam identification

--- src/text_processor.py
+++ src/text_processor.py
@@ -1,7 +1,11 @@
 import string
 import collections
+import re
 
 import nltk
+
+
+EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')
 
 
 class TextProcessor():
@@ -19,7 +23,7 @@ class TextProcessor():
         vectors = list()
         for sentence in sentences:
             words = nltk.word_tokenize(sentence, language=self.lang)
-            words = self.filter_words(words)
+            words = [x.lower() for x in self.filter_words(words)]
             words = collections.Counter(words)
             vectors.append(words)
         return vectors
@@ -28,6 +32,31 @@ class TextProcessor():
         return filter(
             lambda x: x not in self.stopwords and x not in string.punctuation,
             words)
+
+    def compute_spam_score(self, sentence, vector):
+        """Score goes between 0 and 100, 0 is spam, 100 is ham"""
+        score = 100
+        spam_words_malus = 30
+
+        spam_words = ('pute', 'tg', 'gueule')
+        for spam_word in spam_words:
+            if spam_word in vector:
+                score -= spam_words_malus
+
+        # Check if sentence contains a link or an email
+        # We do not need a very strict verification
+        if 'http://' in sentence or 'https://' in sentence:
+            return 100
+        if EMAIL_REGEX.match(sentence):
+            return 100
+
+        # Bound score between 0 and 100
+        if score <= 0:
+            return 0
+        elif score >= 100:
+            return 100
+
+        return score
 
 
 if __name__ == '__main__':
@@ -40,4 +69,6 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         lang = sys.argv[2]
     d = TextProcessor(sys.argv[1], lang)
-    print(d.vectors)
+    for sentence, vector in d.vectors.items():
+        print("{0}\n\t{1}\n\tScore: {2}".format(
+            sentence, vector, d.compute_spam_score(sentence, vector)))
