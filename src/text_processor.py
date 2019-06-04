3906face0a7abac186cc0417c405ea7d7c534d1c
Sugi <sugiuro.dev@gmail.com>
2019-06-04 14:53:38 +0900

    clean stopwords and punctuations, can now choose lang in test main

--- src/text_processor.py
+++ src/text_processor.py
@@ -1,12 +1,15 @@
+import string
 import collections
 
 import nltk
 
 
 class TextProcessor():
-    def __init__(self, text):
+    def __init__(self, text, lang='french'):
         self.source_text = text
-        self.sentences = nltk.tokenize.sent_tokenize(text, language='french')
+        self.lang = lang
+        self.stopwords = nltk.corpus.stopwords.words(self.lang)
+        self.sentences = nltk.tokenize.sent_tokenize(text, language=self.lang)
         self.vectors = dict(zip(self.sentences,
                                 self.compute_vectors(self.sentences)))
 
@@ -15,16 +18,26 @@ class TextProcessor():
         Returns a list of collections.Counter"""
         vectors = list()
         for sentence in sentences:
-            words = nltk.word_tokenize(sentence, language='french')
+            words = nltk.word_tokenize(sentence, language=self.lang)
+            words = self.filter_words(words)
             words = collections.Counter(words)
             vectors.append(words)
         return vectors
 
+    def filter_words(self, words):
+        return filter(
+            lambda x: x not in self.stopwords and x not in string.punctuation,
+            words)
+
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: {0} sentence".format(sys.argv[0]))
         sys.exit(1)
-    d = TextProcessor(sys.argv[1])
+
+    lang = 'french'
+    if len(sys.argv) == 3:
+        lang = sys.argv[2]
+    d = TextProcessor(sys.argv[1], lang)
     print(d.vectors)
